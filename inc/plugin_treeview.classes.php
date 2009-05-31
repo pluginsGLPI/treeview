dfd9e832c74f733a41dc02d7be1eb4468ceabd3c
Xavier CAILLAUD <tsmr@users.noreply.github.com>
2009-05-31 08:58:56 +0000

    [treeview] modify class name function to __construct

--- inc/plugin_treeview.classes.php
+++ inc/plugin_treeview.classes.php
@@ -38,7 +38,7 @@
  */
 class plugin_treeview_display extends CommonDBTM
 {
-	function plugin_treeview_display()
+	function __construct()
 	{
 		$this->table="glpi_plugin_treeview_display";
 	}
@@ -49,7 +49,7 @@ class plugin_treeview_display extends CommonDBTM
  */
 class plugin_treeview_preference extends CommonDBTM
 {
-	function plugin_treeview_preference()
+	function __construct()
 	{
 		$this->table="glpi_plugin_treeview_preference";
 		$this->type=-1;
@@ -159,7 +159,7 @@ class plugin_treeview_Treeview_Config
 
 class plugin_treeview_Profile extends CommonDBTM {
 
-	function plugin_treeview_Profile () {
+	function __construct () {
 		$this->table="glpi_plugin_treeview_profiles";
 		$this->type=-1;
 	}
