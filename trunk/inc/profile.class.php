7445cda327b3797840bb759ae47c36163f530a7e
yllen <yllen@349b9182-4a13-0410-896f-e5e9767dd1b3>
2011-01-31 16:11:25 +0000

    clean code

--- trunk/inc/profile.class.php
+++ trunk/inc/profile.class.php
@@ -73,7 +73,7 @@ class PluginTreeviewProfile extends CommonDBTM {
 //   require 'preference.class.php';
 
       $Pref = new PluginTreeviewPreference();
-      $pref_value = $Pref->checkPreferenceValue($_SESSION['glpiID']);
+      $pref_value = $Pref->checkPreferenceValue(getLoginUserID());
       if ($pref_value==1) {
          $_SESSION["glpi_plugin_treeview_preference"] = 1;
       } else {
