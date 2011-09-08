55e6f2eb8d8fc8bf4dc6fb26265c575fd8588a09
yllen <12936722+yllen@users.noreply.github.com>
2011-09-08 08:28:05 +0000

    new tabs management

--- inc/profile.class.php
+++ inc/profile.class.php
@@ -85,7 +85,7 @@ class PluginTreeviewProfile extends CommonDBTM {
    /**
     * profiles modification
    **/
-   function showForm($id, $options) {
+   function showForm($id, $options=array()) {
       global $LANG;
 
       $target = $this->getFormURL();
@@ -130,6 +130,29 @@ class PluginTreeviewProfile extends CommonDBTM {
       $plugprof = new self();
       $plugprof->delete(array('id' => $prof->getField("id")));
    }
-}
 
+
+   function getTabNameForItem(CommonGLPI $item, $withtemplate=0) {
+      global $LANG;
+
+      if ($item->getType() == 'Profile') {
+         return $LANG['plugin_treeview']['title'][0];
+      }
+      return '';
+   }
+
+
+   static function displayTabContentForItem(CommonGLPI $item, $tabnum=1, $withtemplate=0) {
+
+      if ($item->getType() == 'Profile') {
+         $prof = new self();
+         $ID = $item->getField('id');
+         if (!$prof->GetfromDB($ID)) {
+            $prof->createAccess($item);
+         }
+         $prof->showForm($ID);
+      }
+      return true;
+   }
+}
 ?>
