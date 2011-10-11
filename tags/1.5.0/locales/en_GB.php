f4fe8ab30a8ac741c512c8b10b98c427e50a41a0
remi <remi@349b9182-4a13-0410-896f-e5e9767dd1b3>
2011-10-11 15:18:35 +0000

    create correct tag for 1.5.0

---
+++ tags/1.5.0/locales/en_GB.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * @version $Id: HEADER 1 2009-09-21 14:58 Tsmr $
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2009 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------
+// Original Author of file: CAILLAUD Xavier & AL-RUBEIY Hussein
+// Purpose of file: plugin treeview v1.3.0 - GLPI 0.80
+// ----------------------------------------------------------------------
+ */
+
+$LANG['plugin_treeview']['title'][0] = "Tree View";
+
+$LANG['plugin_treeview']['profile'][0] = "Rights management";
+$LANG['plugin_treeview']['profile'][1] = "Use the tree";
+
+$LANG['plugin_treeview']['setup'][1]  = "Display";
+$LANG['plugin_treeview']['setup'][2]  = "Yes";
+$LANG['plugin_treeview']['setup'][3]  = "No";
+$LANG['plugin_treeview']['setup'][4]  = "Target for all the nodes";
+$LANG['plugin_treeview']['setup'][5]  = "Should folders be links";
+$LANG['plugin_treeview']['setup'][6]  = "Nodes can be highlighted";
+$LANG['plugin_treeview']['setup'][7]  = "Tree is drawn with lines";
+$LANG['plugin_treeview']['setup'][8]  = "Tree is drawn with icons";
+$LANG['plugin_treeview']['setup'][9]  = "Only one node within a parent<br>can be expanded at the same time.";
+$LANG['plugin_treeview']['setup'][10] = "New window";
+$LANG['plugin_treeview']['setup'][11] = "The central console";
+$LANG['plugin_treeview']['setup'][12] = "Item name";
+$LANG['plugin_treeview']['setup'][13] = "Location name";
+$LANG['plugin_treeview']['setup'][14] = "Name";
+$LANG['plugin_treeview']['setup'][15] = "Inventory number";
+$LANG['plugin_treeview']['setup'][16] = "Short name";
+$LANG['plugin_treeview']['setup'][17] = "Long name";
+$LANG['plugin_treeview']['setup'][18] = "Comment";
+$LANG['plugin_treeview']['setup'][19] = "Launch the plugin Treeview with GLPI launching";
+$LANG['plugin_treeview']['setup'][20] = "Warning : If there are more than one plugin which be loaded at startup, then only the first will be used";
+
+?>
---
+++ tags/1.5.0/locales/fr_FR.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * @version $Id: HEADER 1 2009-09-21 14:58 Tsmr $
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2009 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------
+// Original Author of file: CAILLAUD Xavier & AL-RUBEIY Hussein
+// Purpose of file: plugin treeview v1.3.0 - GLPI 0.80
+// ----------------------------------------------------------------------
+ */
+
+$LANG['plugin_treeview']['title'][0] = "Arborescence";
+
+$LANG['plugin_treeview']['profile'][0] = "Gestion des droits";
+$LANG['plugin_treeview']['profile'][1] = "Utiliser l'arborescence";
+
+$LANG['plugin_treeview']['setup'][1]  = "Affichage";
+$LANG['plugin_treeview']['setup'][2]  = "Oui";
+$LANG['plugin_treeview']['setup'][3]  = "Non";
+$LANG['plugin_treeview']['setup'][4]  = "Ouverture des liens";
+$LANG['plugin_treeview']['setup'][5]  = "Utiliser les répertoires des lieux sous forme de liens";
+$LANG['plugin_treeview']['setup'][6]  = "Les noeuds peuvent être en surbrillance";
+$LANG['plugin_treeview']['setup'][7]  = "L'arborescence est dessinée avec des lignes";
+$LANG['plugin_treeview']['setup'][8]  = "L'arborescence est dessinée avec des icônes";
+$LANG['plugin_treeview']['setup'][9]  = "Seulement un noeud avec un parent <br> peut être affiché en même temps";
+$LANG['plugin_treeview']['setup'][10] = "Dans une nouvelle fenêtre";
+$LANG['plugin_treeview']['setup'][11] = "Dans la console centrale";
+$LANG['plugin_treeview']['setup'][12] = "Affichage du matériel";
+$LANG['plugin_treeview']['setup'][13] = "Affichage du lieu";
+$LANG['plugin_treeview']['setup'][14] = "Nom";
+$LANG['plugin_treeview']['setup'][15] = "Numéro d'inventaire";
+$LANG['plugin_treeview']['setup'][16] = "Nom court";
+$LANG['plugin_treeview']['setup'][17] = "Nom complet";
+$LANG['plugin_treeview']['setup'][18] = "Commentaires";
+$LANG['plugin_treeview']['setup'][19] = "Lancer le plugin Treeview au démarrage de GLPI";
+$LANG['plugin_treeview']['setup'][20] = "Attention : si plusieurs plugins sont lancés au démarrage, seul le premier sera actif";
+
+?>
