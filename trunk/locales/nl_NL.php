62c3143e16355b3aefd146ba3c9b037d0bb1e1dc
remi <remi@349b9182-4a13-0410-896f-e5e9767dd1b3>
2011-10-11 15:25:18 +0000

    Dutch translation by Sander Middelhof, fixed #3194

---
+++ trunk/locales/nl_NL.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2009 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------
+// Original Author of file: CAILLAUD Xavier & AL-RUBEIY Hussein
+// Purpose of file: plugin treeview v1.3.0 - GLPI 0.80
+// ----------------------------------------------------------------------
+ */
+
+// Dutch translation by Sander Middelhof
+
+$LANG['plugin_treeview']['title'][0] = "Tree View";
+
+$LANG['plugin_treeview']['profile'][0] = "Rechten management";
+$LANG['plugin_treeview']['profile'][1] = "Gebruik de tree";
+
+$LANG['plugin_treeview']['setup'][1]  = "Overzicht";
+$LANG['plugin_treeview']['setup'][2]  = "Ja";
+$LANG['plugin_treeview']['setup'][3]  = "Nee";
+$LANG['plugin_treeview']['setup'][4]  = "Doel voor alle nodes";
+$LANG['plugin_treeview']['setup'][5]  = "Moeten folders worden gelinkt?";
+$LANG['plugin_treeview']['setup'][6]  = "Nodes kunnen worden gemarkeerd";
+$LANG['plugin_treeview']['setup'][7]  = "Tree wordt getekend met lijnen";
+$LANG['plugin_treeview']['setup'][8]  = "Tree wordt getekend met iconen";
+$LANG['plugin_treeview']['setup'][9]  = "Slechts &eacute&eacuten node binnen een ouder<br>kan worden uitgeklapt.";
+$LANG['plugin_treeview']['setup'][10] = "Nieuw venster";
+$LANG['plugin_treeview']['setup'][11] = "Het centrale console";
+$LANG['plugin_treeview']['setup'][12] = "Item naam";
+$LANG['plugin_treeview']['setup'][13] = "Locatie naam";
+$LANG['plugin_treeview']['setup'][14] = "Naam";
+$LANG['plugin_treeview']['setup'][15] = "Voorraadnummer";
+$LANG['plugin_treeview']['setup'][16] = "Verkorte naam";
+$LANG['plugin_treeview']['setup'][17] = "Lange naam";
+$LANG['plugin_treeview']['setup'][18] = "Opmerking";
+$LANG['plugin_treeview']['setup'][19] = "Start de plugin Treeview op als GLPI start";
+$LANG['plugin_treeview']['setup'][20] = "Let op: Als er meer dan &eacute&eacuten plugin geladen wordt tijdens het opstarten wordt alleen de eerste gebruikt.";
+
+?>
