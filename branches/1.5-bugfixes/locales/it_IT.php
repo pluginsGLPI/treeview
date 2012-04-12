eb29f94d98d405a9ac39b5276ba080dc2120cc4b
yllen <yllen@349b9182-4a13-0410-896f-e5e9767dd1b3>
2012-04-12 17:49:28 +0000

    [1.5.1] Italian translation by Tiziano Sartori - see #3480

---
+++ branches/1.5-bugfixes/locales/it_IT.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * @version $Id: HEADER 1 2012-04-05 14:58 Tsmr $
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2009 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------
+// Original Author of file: SARTORI Tiziano
+// Purpose of file: plugin treeview v1.3.0 - GLPI 0.80
+// ----------------------------------------------------------------------
+ */
+
+$LANG['plugin_treeview']['title'][0] = "Visualizzazione struttura";
+
+$LANG['plugin_treeview']['profile'][0] = "Gestione permessi";
+$LANG['plugin_treeview']['profile'][1] = "Usa la visualizzazione struttura";
+
+$LANG['plugin_treeview']['setup'][1]  = "Visualizza";
+$LANG['plugin_treeview']['setup'][2]  = "Si";
+$LANG['plugin_treeview']['setup'][3]  = "No";
+$LANG['plugin_treeview']['setup'][4]  = "I nodi sono collegati agli oggetti";
+$LANG['plugin_treeview']['setup'][5]  = "Le cartelle sono esposte come links";
+$LANG['plugin_treeview']['setup'][6]  = "I nodi possono essere selezionati";
+$LANG['plugin_treeview']['setup'][7]  = "La struttura è visualizzata ad albero";
+$LANG['plugin_treeview']['setup'][8]  = "La struttura è visualizzata con delle icone";
+$LANG['plugin_treeview']['setup'][9]  = "Solo un nodo con voce padre<br>può essere esploso per volta.";
+$LANG['plugin_treeview']['setup'][10] = "Nuova finestra";
+$LANG['plugin_treeview']['setup'][11] = "Console centrale";
+$LANG['plugin_treeview']['setup'][12] = "Nome dell'oggetto";
+$LANG['plugin_treeview']['setup'][13] = "Posizione";
+$LANG['plugin_treeview']['setup'][14] = "Nome";
+$LANG['plugin_treeview']['setup'][15] = "Matricola inventario";
+$LANG['plugin_treeview']['setup'][16] = "Nome breve";
+$LANG['plugin_treeview']['setup'][17] = "Nome completo";
+$LANG['plugin_treeview']['setup'][18] = "Commenti";
+$LANG['plugin_treeview']['setup'][19] = "Lancia il plugin Treeview quanto viene avviato GLPI";
+$LANG['plugin_treeview']['setup'][20] = "Attenzione : Se viene caricato più di un plugin all'avvio, verrà utilizzato solo il primo";
+
+?>
