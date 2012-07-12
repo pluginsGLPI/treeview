ffabd8de76940c1a2f416dd634f361a692f50d7d
yllen <yllen@349b9182-4a13-0410-896f-e5e9767dd1b3>
2012-07-12 17:13:15 +0000

    TAG 1.6.1

---
+++ tags/1.6.1/locales/fr_FR.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * @version $Id: setup.php 313 2011-12-19 09:39:58Z remi $
+ -------------------------------------------------------------------------
+ treeview - TreeView browser plugin for GLPI
+ Copyright (C) 2003-2012 by the treeview Development Team.
+
+ https://forge.indepnet.net/projects/treeview
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of treeview.
+
+ treeview is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ treeview is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with treeview. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+$LANG['plugin_treeview']['title'][0] = "Arborescence";
+
+$LANG['plugin_treeview']['profile'][0] = "Gestion des droits";
+$LANG['plugin_treeview']['profile'][1] = "Utiliser l'arborescence";
+
+$LANG['plugin_treeview']['setup'][1]  = "Affichage";
+$LANG['plugin_treeview']['setup'][2]  = "Oui";
+$LANG['plugin_treeview']['setup'][3]  = "Non";
+$LANG['plugin_treeview']['setup'][4]  = "Ouverture des liens";
+$LANG['plugin_treeview']['setup'][5]  = "Utiliser les répertoires des lieux sous forme de liens";
+$LANG['plugin_treeview']['setup'][6]  = "Les noeuds peuvent être en surbrillance";
+$LANG['plugin_treeview']['setup'][7]  = "L'arborescence est dessinée avec des lignes";
+$LANG['plugin_treeview']['setup'][8]  = "L'arborescence est dessinée avec des icônes";
+$LANG['plugin_treeview']['setup'][9]  = "Seulement un noeud avec un parent <br> peut être affiché en même temps";
+$LANG['plugin_treeview']['setup'][10] = "Dans une nouvelle fenêtre";
+$LANG['plugin_treeview']['setup'][11] = "Dans la console centrale";
+$LANG['plugin_treeview']['setup'][12] = "Affichage du matériel";
+$LANG['plugin_treeview']['setup'][13] = "Affichage du lieu";
+$LANG['plugin_treeview']['setup'][14] = "Nom";
+$LANG['plugin_treeview']['setup'][15] = "Numéro d'inventaire";
+$LANG['plugin_treeview']['setup'][16] = "Nom court";
+$LANG['plugin_treeview']['setup'][17] = "Nom complet";
+$LANG['plugin_treeview']['setup'][18] = "Commentaires";
+$LANG['plugin_treeview']['setup'][19] = "Lancer le plugin Treeview au démarrage de GLPI";
+$LANG['plugin_treeview']['setup'][20] = "Attention : si plusieurs plugins sont lancés au démarrage, seul le premier sera actif";
+
+?>
