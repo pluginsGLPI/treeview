ee322e5a3d12388a3406d94791bb9d2272a2e8d9
Remi Collet <270445+remicollet@users.noreply.github.com>
2013-01-27 16:48:09 +0000

    GLPI_ROOT is now defined in include.php

--- front/config.form.php
+++ front/config.form.php
@@ -27,11 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-if (!defined('GLPI_ROOT')) {
-   define('GLPI_ROOT', '../../..');
-}
-
-include (GLPI_ROOT . "/inc/includes.php");
+include ('../../../inc/includes.php');
 
 $config = new PluginTreeviewConfig();
 if (isset($_POST["update"])) {
--- front/profile.form.php
+++ front/profile.form.php
@@ -27,11 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-if (!defined('GLPI_ROOT')) {
-   define('GLPI_ROOT', '../../..');
-}
-
-include (GLPI_ROOT."/inc/includes.php");
+include ('../../../inc/includes.php');
 
 Session::checkRight("profile", "r");
 
