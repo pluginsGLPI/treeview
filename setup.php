076f64b20ee40354de902b51d0da16216b7a0e59
Remi Collet <270445+remicollet@users.noreply.github.com>
2013-02-28 08:15:55 +0000

    use roo_doc in URL (not GLPI_ROOT)

--- setup.php
+++ setup.php
@@ -70,7 +70,7 @@ function plugin_init_treeview() {
           && isset($_SESSION["glpi_plugin_treeview_preference"])
           && $_SESSION["glpi_plugin_treeview_preference"] == 1) {
 
-            Html::redirect(GLPI_ROOT."/plugins/treeview/index.php");
+            Html::redirect($CFG_GLPI["root_doc"]."/plugins/treeview/index.php");
       }
 
       if ($_SERVER['PHP_SELF'] == $CFG_GLPI["root_doc"]."/logout.php"
